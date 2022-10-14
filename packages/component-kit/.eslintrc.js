16b4aadb9a87df72351b9f04f05d5dca6b6515b6
Xander Low <alexanderjameslow@gmail.com>
2022-10-14 16:04:38 -0700

    initial commit

---
+++ packages/component-kit/.eslintrc.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers");
+
+/** @type {import("eslint").Linter.Config} */
+const eslintConfig = {
+  extends: [
+    require.resolve("@strangelovelabs/style-guide/eslint/browser-node"),
+    require.resolve("@strangelovelabs/style-guide/eslint/react"),
+    require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
+  ],
+  parserOptions: {
+    project: getTsconfigPath(),
+  },
+  root: true,
+};
+
+module.exports = eslintConfig;
