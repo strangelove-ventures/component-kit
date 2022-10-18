a2af4ce2268cc056ffc4074a2b57c2a514a26d42
Xander Low <alexanderjameslow@gmail.com>
2022-10-18 13:03:18 -0700

    added: color palettes

---
+++ packages/component-kit/src/theme/colors.ts
@@ -0,0 +1,108 @@
+export const colors: Record<string, Record<string | number, string> | string> = {
+  ghostGreen: "#d6ffdc",
+  altShade: "#29302a",
+  spaceBlack: "#191919",
+  backgroundGray: "#252725",
+  alertRed: "#ab1b1b",
+  confirmGreen: "#0ec829",
+  warningYellow: "#f8e755",
+  jetfuelOrange: "#f07d4a",
+  afterburnerBlue: "#4dddfa",
+  web3Purple: "#8b4da9",
+  primary: {
+    50: "#f3fff5",
+    100: "#e6ffea",
+    200: "#d6ffdc", // ghost green
+    300: "#c1e6c6",
+    400: "#abccb0",
+    500: "#809984",
+    600: "#6b806e",
+    700: "#566658",
+    800: "#404c42",
+    900: "#29302a", // brand alt shade
+  },
+  gray: {
+    50: "#a3a3a3",
+    100: "#7e7e7e",
+    200: "#6c6c6c",
+    300: "#404040",
+    400: "#323232",
+    500: "#242424",
+    600: "#1c1c1c",
+    700: "#252725", // background gray
+    800: "#191919", // space black
+    900: "#0a0a0a",
+  },
+  red: {
+    50: "#dda4a4",
+    100: "#cd7676",
+    200: "#c45f5f",
+    300: "#bc4949",
+    400: "#b33232",
+    500: "#ab1b1b", // alert red
+    600: "#891616",
+    700: "#671010",
+    800: "#440b0b",
+    900: "#330808",
+  },
+  green: {
+    50: "#cff4d4",
+    100: "#9fe9a9",
+    200: "#87e494",
+    300: "#6ede7f",
+    400: "#3ed354",
+    500: "#0ec829", // confirm green
+    600: "#0ba021",
+    700: "#087819",
+    800: "#065010",
+    900: "#032808",
+  },
+  yellow: {
+    50: "#fcf3aa",
+    100: "#fbf199",
+    200: "#faee88",
+    300: "#f9ec77",
+    400: "#f9e966",
+    500: "#f8e755", // warning yellow
+    600: "#dfd04d",
+    700: "#c6b944",
+    800: "#958b33",
+    900: "#635c22",
+  },
+  orange: {
+    50: "#f8bea5",
+    100: "#f6b192",
+    200: "#f5a480",
+    300: "#f3976e",
+    400: "#f28a5c",
+    500: "#f07d4a", // jetfuel orange
+    600: "#c0643b",
+    700: "#904b2c",
+    800: "#783f25",
+    900: "#60321e",
+  },
+  blue: {
+    50: "#caf5fe",
+    100: "#b8f1fd",
+    200: "#a6eefd",
+    300: "#82e7fc",
+    400: "#71e4fb",
+    500: "#4dddfa", // afterburner blue
+    600: "#3eb1c8",
+    700: "#2e8596",
+    800: "#1f5864",
+    900: "#0f2c32",
+  },
+  purple: {
+    50: "#c5a6d4",
+    100: "#b994cb",
+    200: "#ae82c3",
+    300: "#a271ba",
+    400: "#975fb2",
+    500: "#8b4da9", // web3 purple
+    600: "#6f3e87",
+    700: "#532e65",
+    800: "#2a1733",
+    900: "#1c0f22",
+  },
+};
--- packages/component-kit/src/theme/index.ts
+++ packages/component-kit/src/theme/index.ts
@@ -1,3 +1,4 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from "@chakra-ui/react";
+import { colors } from "./colors";
 
-export const theme = extendTheme({})
+export const theme = extendTheme({ colors });
