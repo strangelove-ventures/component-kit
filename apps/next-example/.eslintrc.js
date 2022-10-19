7b1d8414c18540b49ad85465a0f3322ebff20f9b
Xander Low <alexanderjameslow@gmail.com>
2022-10-19 13:14:31 -0700

    updated import order

--- apps/next-example/.eslintrc.js
+++ apps/next-example/.eslintrc.js
@@ -5,9 +5,9 @@ const { getTsconfigPath } = require("@strangelovelabs/style-guide/eslint/helpers
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
   extends: [
-    require.resolve("@strangelovelabs/style-guide/eslint/next"),
-    require.resolve("@strangelovelabs/style-guide/eslint/react"),
     require.resolve("@strangelovelabs/style-guide/eslint/typescript"),
+    require.resolve("@strangelovelabs/style-guide/eslint/react"),
+    require.resolve("@strangelovelabs/style-guide/eslint/next"),
   ],
   parserOptions: {
     project: getTsconfigPath(),
