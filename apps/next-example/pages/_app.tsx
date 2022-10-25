659cc34b5e01b739a88d697821b10d5e4a01e1fb
Xander Low <alexanderjameslow@gmail.com>
2022-10-24 17:06:09 -0700

    added: fonts

--- apps/next-example/pages/_app.tsx
+++ apps/next-example/pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@strangelovelabs/component-kit";
+import "@fontsource/dm-sans";
+import "@fontsource/recursive";
+import "@fontsource/ibm-plex-mono";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
